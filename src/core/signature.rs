3a3e4251ec1c86eb41325603dad0107d69e9b206
jack <consor_inmarsat@aleeas.com>
2023-12-17 09:36:08 -0800

    add methods, tests

--- src/core/signature.rs
+++ src/core/signature.rs
@@ -1,4 +1,4 @@
-use super::{NanoError, try_point_from_slice};
+use super::{NanoError, Key, Account, try_point_from_slice};
 use zeroize::Zeroize;
 use curve25519_dalek::{EdwardsPoint, Scalar as RawScalar};
 
@@ -15,6 +15,14 @@ pub struct Signature {
 impl Signature {
     pub fn to_bytes(&self) -> [u8; 64] {
         (*self).into()
+    }
+
+    pub fn new(message: &[u8], key: &Key) -> Signature {
+        key.sign_message(message)
+    }
+
+    pub fn is_valid(&self, message: &[u8], account: &Account) -> bool {
+        account.is_valid_signature(message, *self)
     }
 }
 impl From<Signature> for [u8; 64] {
@@ -34,5 +42,49 @@ impl TryFrom<[u8; 64]> for Signature {
             value[32..64].try_into().unwrap()
         );
         Ok(Signature{r, s})
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{SecretBytes, Key};
+
+    fn get_key(seed: [u8; 32], i: u32) -> Key {
+        let seed = SecretBytes::from(&mut seed.clone());
+        Key::from_seed(&seed, i)
+    }
+
+    #[test]
+    fn valid() {
+        let key = get_key([0; 32], 0);
+        let account = key.to_account();
+        let signature = key.sign_message(b"test");
+        assert!(account.is_valid_signature(b"test", signature))
+    }
+
+    #[test]
+    fn invalid_key() {
+        let key = get_key([0; 32], 0);
+        let account = get_key([0; 32], 1).to_account();
+        let signature = key.sign_message(b"test");
+        assert!(!account.is_valid_signature(b"test", signature))
+    }
+
+    #[test]
+    fn invalid_message() {
+        let key = get_key([0; 32], 0);
+        let account = key.to_account();
+        let signature = key.sign_message(b"test 1");
+        assert!(!account.is_valid_signature(b"test 2", signature))
+    }
+
+    #[test]
+    fn r_safety() {
+        let key = get_key([0; 32], 0);
+        let signature_1 = key.sign_message(b"test 1");
+        let signature_2 = key.sign_message(b"test 2");
+        assert!(signature_1 != signature_2);
+        assert!(signature_1.r != signature_2.r);
+        assert!(signature_1.s != signature_2.s);
     }
 }
